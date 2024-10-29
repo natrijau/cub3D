9d9389ef8fbee5c618977e1da07db6e8991f15c9
L0live <yann.olive@outlook.com>
2024-10-29 15:31:45 +0100

    fix collisions at corner

--- main.c
+++ main.c
@@ -65,7 +65,10 @@ void	moove(t_data *data, int y, int x)
 	new_y = (sin(data->angle) * MOOVE_SPEED) * x;
 	new_x += (cos(data->angle + W) * MOOVE_SPEED) * y;
 	new_y += (sin(data->angle + W) * MOOVE_SPEED) * y;
-	if (data->map[(int)(data->y + new_y) / CASE][(int)(data->x + new_x) / CASE] != '1')
+	if (data->map[(int)(data->y + new_y) / CASE][(int)(data->x + new_x) / CASE] != '1'
+		&& data->map[(int)(data->y + new_y + 1) / CASE][(int)(data->x + new_x + 1) / CASE] != '1'
+		&& data->map[(int)(data->y + new_y + 1) / CASE][(int)(data->x + new_x) / CASE] != '1'
+		&& data->map[(int)(data->y + new_y) / CASE][(int)(data->x + new_x + 1) / CASE] != '1')
 	{
 		data->x += new_x;  // Update x position
 		data->y += new_y;  // Update y position
