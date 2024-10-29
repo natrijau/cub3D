7f16a0153e82d10485c0b3505428dc96dd91b6ef
L0live <yann.olive@outlook.com>
2024-10-29 15:35:59 +0100

    same fix collisions

--- main.c
+++ main.c
@@ -68,7 +68,10 @@ void	moove(t_data *data, int y, int x)
 	if (data->map[(int)(data->y + new_y) / CASE][(int)(data->x + new_x) / CASE] != '1'
 		&& data->map[(int)(data->y + new_y + 1) / CASE][(int)(data->x + new_x + 1) / CASE] != '1'
 		&& data->map[(int)(data->y + new_y + 1) / CASE][(int)(data->x + new_x) / CASE] != '1'
-		&& data->map[(int)(data->y + new_y) / CASE][(int)(data->x + new_x + 1) / CASE] != '1')
+		&& data->map[(int)(data->y + new_y) / CASE][(int)(data->x + new_x + 1) / CASE] != '1'
+		&& data->map[(int)(data->y + new_y + 2) / CASE][(int)(data->x + new_x + 2) / CASE] != '1'
+		&& data->map[(int)(data->y + new_y + 2) / CASE][(int)(data->x + new_x) / CASE] != '1'
+		&& data->map[(int)(data->y + new_y) / CASE][(int)(data->x + new_x + 2) / CASE] != '1')
 	{
 		data->x += new_x;  // Update x position
 		data->y += new_y;  // Update y position
